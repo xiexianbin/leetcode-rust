01961329c78f41f392745935151ad80cae44be6f
xiexianbin <me@xiexianbin.cn>
2022-08-16 11:12:47 +0800

    init
    Signed-off-by: xiexianbin <me@xiexianbin.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,32 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i, x) in nums.iter().enumerate() {
+            for (j, y) in nums.iter().enumerate() {
+                if i != j {
+                    if target == x + y {
+                        let mut v: Vec<i32> = Vec::new();
+                        v.push(i as i32);
+                        v.push(j as i32);
+                        return v
+                    }
+                }
+            }
+        }
+
+        return vec![0, 0];        
+    }
+}
+
+fn main() {
+    let nums = vec![2,7,11,15];
+    let target = 9;
+    println!("{:?}", Solution::two_sum(nums, target));
+}
